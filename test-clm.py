f43db84f04caaea451a7f515996b9ae8454e2dd8
Ben Andre <bandre@lbl.gov>
2014-07-14 22:41:40 -0600

    always compare componets for clm history files, fix a bunch of stuff to get working on yellowstone

--- test-clm.py
+++ test-clm.py
@@ -39,7 +39,7 @@ else:
 
 aux_clm = Template("""
 $batch ./create_test -xml_category $suite \
-$generate $baseline \
+$generate $baseline -model_gen_comp clm2 \
 -baselineroot $baseline_root -testroot $test_root \
 -xml_mach $machine -xml_compiler $compiler -testid  $testid
 """
@@ -91,7 +91,7 @@ def read_config_file(cfg_file):
     [yellowstone]
     host=yslogin
     BATCH=execca
-    BACKGRLOUND=
+    BACKGRLOUND=&
     CESM_INPUTDATA=/glade/p/cesm/cseg/inputdata
     BASELINE_ROOT/=glade/p/cseg/ccsm_baselines
     SCRATCH_DIR=/glade/scratch/andre
@@ -119,25 +119,29 @@ def read_config_file(cfg_file):
             config_dict[s][key] = value
     return config_dict
 
-def run_command(command, logfile, dry_run):
+def run_command(command, logfile, background, dry_run):
     """Generic function to run a shell command, with timout limit, and
     append output to a log file.
 
     """
+    cmd_status = 0
     print("-"*80)
     print(" ".join(command))
     if dry_run:
-        return 0
+        return cmd_status
     try:
-        with open(logfile, 'a') as run_stdout:
+        with open(logfile, 'w') as run_stdout:
             start = time.time()
             proc = subprocess.Popen(command,
                                     shell=False,
                                     stdout=run_stdout,
                                     stderr=subprocess.STDOUT)
-            while proc.poll() is None:
-                time.sleep(10.0)
-            cmd_status = abs(proc.returncode)
+            print("\nstarted as pid : {0}".format(proc.pid), file=run_stdout)
+            print("\nstarted as pid : {0}".format(proc.pid))
+            if not background:
+                while proc.poll() is None:
+                    time.sleep(10.0)
+                cmd_status = abs(proc.returncode)
     except Exception as e:
         print("ERROR: Running command :\n    '{0}'".format(" ".join(command)))
         print(e)
@@ -147,10 +151,10 @@ def run_command(command, logfile, dry_run):
 
 #------------------------------------------------------------------------------
 
-def get_testid(now):
-    testid = now.strftime("%Y%m%d-%H")
-    print(testid)
-    return(testid)
+def get_timestamp(now):
+    timestamp = now.strftime("%Y%m%d-%H")
+    print(timestamp)
+    return(timestamp)
     
 
 def get_hostname():
@@ -165,47 +169,55 @@ def get_machine(config):
     machine = None
     hostname = get_hostname()
     for m in config:
-        print("({0}, {1})".format(hostname, m))
         if (config[m]["host"] in hostname or
             hostname in config[m]["host"]):
             machine = m
-    print(machine)
+    print("Running on : {0}".format(machine))
     return machine
 
-def write_test_description_file(test_root, compiler, suite, config, baseline_tag):
+def write_test_description_file(test_root, compiler, suite, config, baseline_tag, testid, machine):
     filename = "{test_root}/{suite}.{compiler}.info.txt".format(test_root=test_root, suite=suite, compiler=compiler)
     with open(filename, 'w') as info:
         info.write("scratch_dir = {0}\n".format(config["scratch_dir"]))
         info.write("test_data_dir = {0}\n".format(test_root))
         info.write("baseline = {0}\n".format(baseline_tag))
-        info.write("{compiler}_status = cs.status.XXX.YYY\n".format(compiler=compiler))
+        info.write("{compiler}_status = cs.status.{testid}.{machine}\n".format(compiler=compiler, testid=testid, machine=machine))
+        info.write("expected_fail = {0}/../models/lnd/clm/bld/unit_testers/xFail/expectedClmTestFails.xml\n".format(os.getcwd()))
 
 
-def run_test_suites(machine, config, testid, baseline_tag, dry_run):
+def run_test_suites(machine, config, timestamp, baseline_tag, dry_run):
 
     if config.has_key("compilers"):
         compilers = config["compilers"].split(', ')
-        print(compilers)
+    else:
+        raise RuntimeError("machine config must specify compilers")
 
     if config.has_key("suites"):
         suites = config["suites"].split(", ")
-        print(suites)
+    else:
+        raise RuntimeError("machine config must specify test suites")
+        
 
     test_root = "{0}/tests-{1}".format(config["scratch_dir"],
-                                       testid)
+                                       timestamp)
     if not os.path.isdir(test_root):
         os.mkdir(test_root)
 
     baseline = "-baseline {0}".format(baseline_tag)
 
+    background = False
+    if config["background"].lower().find('t') == 0:
+        background = True
+
     for s in suites:
         for c in compilers:
+            testid = "{0}-{1}-{2}".format(timestamp, s, c)
             command = aux_clm.substitute(config, machine=machine, compiler=c, suite=s,
                                          baseline=baseline, generate='',
                                          test_root=test_root, testid=testid)
-            logfile="{test_root}/{testid}.{suite}.{machine}.{compiler}.clm.tests.out".format(test_root=test_root, testid=testid, suite=s, machine=machine, compiler=c)
-            write_test_description_file(test_root, c, s, config, baseline_tag)
-            run_command(command.split(), logfile, dry_run)
+            logfile="{test_root}/{timestamp}.{suite}.{machine}.{compiler}.clm.tests.out".format(test_root=test_root, timestamp=timestamp, suite=s, machine=machine, compiler=c)
+            write_test_description_file(test_root, c, s, config, baseline_tag, testid, machine)
+            run_command(command.split(), logfile, background, dry_run)
 
 
 #------------------------------------------------------------------------------
@@ -213,12 +225,13 @@ def run_test_suites(machine, config, testid, baseline_tag, dry_run):
 
 def main(options):
     now = datetime.datetime.now()
-    testid = get_testid(now)
+    timestamp = get_timestamp(now)
     config = read_config_file(options.config[0])
     machine = get_machine(config)
     if config.has_key(machine):
-        run_test_suites(machine, config[machine], testid, options.baseline[0], options.dry_run)
-    config[testid] = testid
+        for k in config[machine]:
+            print("  {0} : {1}".format(k, config[machine][k]))
+        run_test_suites(machine, config[machine], timestamp, options.baseline[0], options.dry_run)
 
     return 0
 
